712fbf0fd3aa87c49fe44f8505c7da0e449256bb
Henry Chen <henry.chen.830@gmail.com>
2014-12-16 02:48:00 +0800

    Added NASDAQ and HK stock testcase.

--- WindService/src/test/java/KDataTestCase.java
+++ WindService/src/test/java/KDataTestCase.java
@@ -79,5 +79,50 @@ public class KDataTestCase {
 		List<Map<String, String>> dateRes = ws.getKData("00872.HK", begin, end, KType.MONTH_KTYPE);
 		Assert.assertTrue(dateRes.size() == 0);
 	}
+	
+	
+	@Test
+	public void testNASDAQ() throws IOException, ParseException, InterruptedException, WindErrorResponse {
+		
+		
+		//获取AAL.O的K线信息， 其中KType有六种
+		DateFormat format1 = new SimpleDateFormat(KDATA_DATE_FORMAT);        
+		Date begin = format1.parse("2014-10-17 01:38:31");
+		Date end = format1.parse("2014-11-16 15:38:31");
+		
+		List<Map<String, String>> res =  ws.getKData("AAL.O", begin, end, KType.DAY_KTYPE);
+		Assert.assertTrue(res.size() > 0);
+		Map<String, String> kdata = res.get(0);
+		Assert.assertEquals("20141017", kdata.get("w_time"));
+		Assert.assertEquals("33.88", kdata.get("open"));
+		Assert.assertEquals("15985070.0", kdata.get("volume"));
+		Assert.assertEquals("nan", kdata.get("amt"));
+		Assert.assertEquals("34.34", kdata.get("high"));
+		Assert.assertEquals("32.81", kdata.get("low"));
+		Assert.assertEquals("33.38", kdata.get("close"));
+
+	}
+	
+	@Test
+	public void testHK() throws IOException, ParseException, InterruptedException, WindErrorResponse {
+		
+		
+		//获取0001.HK的K线信息， 其中KType有六种
+		DateFormat format1 = new SimpleDateFormat(KDATA_DATE_FORMAT);        
+		Date begin = format1.parse("2014-11-16 01:38:31");
+		Date end = format1.parse("2014-12-16 15:38:31");
+		
+		List<Map<String, String>> res =  ws.getKData("0001.HK", begin, end, KType.DAY_KTYPE);
+		Assert.assertTrue(res.size() > 0);
+		Map<String, String> kdata = res.get(0);
+		Assert.assertEquals("20141117", kdata.get("w_time"));
+		Assert.assertEquals("142.0", kdata.get("open"));
+		Assert.assertEquals("2206421.0", kdata.get("volume"));
+		Assert.assertEquals("309412240.0", kdata.get("amt"));
+		Assert.assertEquals("142.2", kdata.get("high"));
+		Assert.assertEquals("139.3", kdata.get("low"));
+		Assert.assertEquals("139.5", kdata.get("close"));
+
+	}
 
 }
